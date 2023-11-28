2f9b22caa7eb30901d71e5160686a9c75328a500
Forrest Palmer <palmerforrest@gmail.com>
2023-11-28 14:08:22 -0500

    Added code for commands, context menus, HTML Elements, icons, modals, ribbon actions and settings.

--- main.ts
+++ main.ts
@@ -1,20 +1,148 @@
-import { Plugin } from "obsidian";
+import { Menu, Notice, Editor, moment, addIcon, Plugin, setIcon } from "obsidian";
+import { ExampleModal, ExampleSuggestModal } from "./modals";
+import { ExampleSettingTab } from "./settings";
+
+// The settings definition and default settings
+//
+interface ExamplePluginSettings {
+	dateFormat: string;
+}
+const DEFAULT_SETTINGS: Partial<ExamplePluginSettings> = {
+	dateFormat: "YYYY-MM-DD",
+};
 
 export default class ExamplePlugin extends Plugin {
 	statusBarTextElement: HTMLSpanElement;
+	settings: ExamplePluginSettings;
 
-	onload() {
+	async onload() {
+
+		// Add the Settings Tab
+		//
+		await this.loadSettings();
+		this.addSettingTab(new ExampleSettingTab(this.app, this));
+
+		// Add a line count to the status bar
+		//
 		this.statusBarTextElement = this.addStatusBarItem().createEl('span')
-		this.readActiveFileAndUpdateLineCount();
+		await this.readActiveFileAndUpdateLineCount();
 
 		this.app.workspace.on("active-leaf-change", async () => {
-			this.readActiveFileAndUpdateLineCount();
+			await this.readActiveFileAndUpdateLineCount();
 		})
 
 		this.app.workspace.on('editor-change', editor => {
 			const content = editor.getDoc().getValue();
 			this.updateLineCount(content);
 		})
+
+		// Add a left ribbon icon and command
+		//
+		this.addRibbonIcon("dollar-sign", "Print to console", () => {
+			console.log("Activating Sports Book");
+		});
+
+		// Add an icon and two spans to the status bar
+		//
+		const fruits = this.addStatusBarItem();
+		setIcon(fruits, "info");
+		fruits.createEl("span", {text: "🍎"});
+		fruits.createEl("span", {text: "🍌"});
+
+		// Insert text into the current editor
+		//
+		this.addCommand({
+			id: "insert-today's-date",
+			name: "Insert today's date",
+			editorCallback: (editor: Editor) => {
+				editor.replaceRange(
+					moment().format("YYYY-MM-DD"),
+					editor.getCursor()
+				);
+			},
+		});
+
+		// Replace the selection in the current editor
+		//
+		this.addCommand({
+			id: "convert-to-uppercase",
+			name: "Convert to uppercase",
+			editorCallback: (editor: Editor) => {
+				const selection = editor.getSelection();
+				editor.replaceSelection(selection.toUpperCase());
+			},
+		});
+
+		// Context Menus
+		//
+		this.addRibbonIcon("dice", "Open menu", (event) => {
+			const menu = new Menu();
+
+			menu.addItem((item) =>
+				item
+					.setTitle("Copy")
+					.setIcon("documents")
+					.onClick(() => {
+						new Notice("Copied");
+					})
+			);
+
+			menu.addItem((item) =>
+				item
+					.setTitle("Paste")
+					.setIcon("paste")
+					.onClick(() => {
+						new Notice("Pasted");
+					})
+			);
+
+			menu.showAtMouseEvent(event);
+		});
+
+		// Add an icon to the
+		addIcon("circle", `<circle cx="50" cy="50" r="50" fill="currentColor" />`);
+
+		this.addRibbonIcon("circle", "Click me", () => {
+			console.log("The circle says Hey, you!!!");
+		});
+
+		// Fun with modals.
+		//
+		// this.addCommand({
+		// 	id: "display-modal",
+		// 	name: "Display modal",
+		// 	callback: () => {
+		// 		new ExampleModal(this.app).open();
+		// 	},
+		// });
+
+		this.addCommand({
+			id: "display-modal",
+			name: "Display modal",
+			callback: () => {
+				new ExampleModal(this.app, (result) => {
+					new Notice(`Hello, ${result}!`);
+				}).open();
+			},
+		});
+
+		this.addCommand({
+			id: "suggest-modal",
+			name: "Suggest modal",
+			callback: () => {
+				new ExampleSuggestModal(this.app).open();
+			},
+		});
+	}
+
+	// Helper methods for the setting code
+	//
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
 	}
 
 	private async readActiveFileAndUpdateLineCount() {
---
+++ modals.ts
@@ -0,0 +1,96 @@
+import {App, Modal, Notice, Setting, SuggestModal} from "obsidian";
+
+interface Book {
+  title: string;
+  author: string;
+}
+
+const ALL_BOOKS = [
+  {
+    title: "How to Take Smart Notes",
+    author: "Sönke Ahrens",
+  },
+  {
+    title: "Thinking, Fast and Slow",
+    author: "Daniel Kahneman",
+  },
+  {
+    title: "Deep Work",
+    author: "Cal Newport",
+  },
+];
+
+export class ExampleSuggestModal extends SuggestModal<Book> {
+  // Returns all available suggestions.
+  getSuggestions(query: string): Book[] {
+    return ALL_BOOKS.filter((book) =>
+      book.title.toLowerCase().includes(query.toLowerCase())
+    );
+  }
+
+  // Renders each suggestion item.
+  renderSuggestion(book: Book, el: HTMLElement) {
+    el.createEl("div", { text: book.title });
+    el.createEl("small", { text: book.author });
+  }
+
+  // Perform action on the selected suggestion.
+  onChooseSuggestion(book: Book, evt: MouseEvent | KeyboardEvent) {
+    new Notice(`Selected ${book.title}`);
+  }
+}
+
+export class ExampleModal extends Modal {
+	result: string;
+	onSubmit: (result: string) => void;
+
+	constructor(app: App, onSubmit: (result: string) => void) {
+		super(app);
+		this.onSubmit = onSubmit;
+	}
+
+	onOpen() {
+		const {contentEl} = this;
+		contentEl.createEl("h1", {text: "What's your name?"});
+
+		new Setting(contentEl)
+			.setName("Name")
+			.addText((text) =>
+				text.onChange((value) => {
+					this.result = value
+				}));
+
+		new Setting(contentEl)
+			.addButton((btn) =>
+				btn
+					.setButtonText("Submit")
+					.setCta()
+					.onClick(() => {
+						this.close();
+						this.onSubmit(this.result);
+					}));
+	}
+
+	onClose() {
+		let {contentEl} = this;
+		contentEl.empty();
+	}
+}
+
+/*
+export class ExampleModal extends Modal {
+  constructor(app: App) {
+    super(app);
+  }
+
+  onOpen() {
+    let { contentEl } = this;
+    contentEl.setText("Look at me, I'm a modal! 👀");
+  }
+
+  onClose() {
+    let { contentEl } = this;
+    contentEl.empty();
+  }
+}
+*/
